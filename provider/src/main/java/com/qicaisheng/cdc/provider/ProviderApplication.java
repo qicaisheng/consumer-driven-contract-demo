c65ddfb525f9564a71b5d408caf2cbda16d23485
qicaisheng <qicaisheng@qq.com>
2017-05-09 15:02:43 +0800

    Add contract test for provider to verify consumer1 contract test.

--- provider/src/main/java/com/qicaisheng/cdc/provider/ProviderApplication.java
+++ provider/src/main/java/com/qicaisheng/cdc/provider/ProviderApplication.java
@@ -2,11 +2,22 @@ package com.qicaisheng.cdc.provider;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class ProviderApplication {
 
-	public static void main(String[] args) {
+    public static void main(String[] args) {
 		SpringApplication.run(ProviderApplication.class, args);
 	}
+
+    @RequestMapping(value = "/students", produces = "application/json;charset=UTF-8")
+    public List<Student> getStudents() {
+        return Arrays.asList(new Student(5, "James"));
+    }
 }
---
+++ provider/src/test/java/com/qicaisheng/cdc/provider/ProviderPactTests.java
@@ -0,0 +1,46 @@
+package com.qicaisheng.cdc.provider;
+
+import au.com.dius.pact.provider.junit.PactRunner;
+import au.com.dius.pact.provider.junit.Provider;
+import au.com.dius.pact.provider.junit.State;
+import au.com.dius.pact.provider.junit.loader.PactFolder;
+import au.com.dius.pact.provider.junit.target.HttpTarget;
+import au.com.dius.pact.provider.junit.target.Target;
+import au.com.dius.pact.provider.junit.target.TestTarget;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.github.restdriver.clientdriver.ClientDriverRule;
+import org.junit.Before;
+import org.junit.ClassRule;
+import org.junit.runner.RunWith;
+
+import java.util.List;
+
+import static com.github.restdriver.clientdriver.RestClientDriver.giveResponse;
+import static com.github.restdriver.clientdriver.RestClientDriver.onRequestTo;
+
+@RunWith(PactRunner.class)
+@Provider("provider")
+@PactFolder("pacts")
+public class ProviderPactTests {
+	@ClassRule
+	public static final ClientDriverRule embeddedService = new ClientDriverRule(8080);
+
+	@State("contract for consumer1")
+	public void toVerifiesConsumer1GettingStudents() {
+	}
+
+	@TestTarget // Annotation denotes Target that will be used for tests
+	public final Target target = new HttpTarget(8080);
+
+	@Before
+	public void before() throws JsonProcessingException {
+		ProviderApplication providerApplication = new ProviderApplication();
+		List<Student> students = providerApplication.getStudents();
+		ObjectMapper mapper = new ObjectMapper();
+		String body = mapper.writeValueAsString(students);
+
+		embeddedService.addExpectation(
+				onRequestTo("/students"), giveResponse(body, "application/json;charset=utf-8"));
+	}
+}
