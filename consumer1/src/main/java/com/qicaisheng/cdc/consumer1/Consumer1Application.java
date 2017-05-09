7c2d3065a0a54479ff255bf3252bbf0d9dfd9d8d
qicaisheng <qicaisheng@qq.com>
2017-05-09 14:26:44 +0800

    Add contract test for consumer1.

--- consumer1/src/main/java/com/qicaisheng/cdc/consumer1/Consumer1Application.java
+++ consumer1/src/main/java/com/qicaisheng/cdc/consumer1/Consumer1Application.java
@@ -2,11 +2,27 @@ package com.qicaisheng.cdc.consumer1;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.List;
 
 @SpringBootApplication
+@RestController
 public class Consumer1Application {
 
 	public static void main(String[] args) {
 		SpringApplication.run(Consumer1Application.class, args);
+	}
+
+	@RequestMapping()
+	public List<Student> getStudents() {
+		ParameterizedTypeReference<List<Student>> parameterizedTypeReference = new ParameterizedTypeReference<List<Student>>() {};
+		String providerUrl = "http://localhost:8080/students";
+
+		return new RestTemplate().exchange(providerUrl, HttpMethod.GET, null, parameterizedTypeReference).getBody();
 	}
 }
---
+++ consumer1/src/test/java/com/qicaisheng/cdc/consumer1/Consumer1PactTests.java
@@ -0,0 +1,49 @@
+package com.qicaisheng.cdc.consumer1;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRule;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.PactFragment;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Consumer1PactTests {
+
+	@Rule
+	public PactProviderRule mockProvider = new PactProviderRule("provider", "localhost", 8080, this);
+
+	@Pact(state = "consumer1 get students", provider="provider", consumer="consumer1")
+	public PactFragment createFragment(PactDslWithProvider builder) throws JsonProcessingException {
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json;charset=utf-8");
+
+		return builder
+				.given("contract for consumer1")
+				.uponReceiving("Get students")
+				.path("/students")
+				.method("GET")
+				.willRespondWith()
+				.status(200)
+				.headers(headers)
+				.body("[{\"id\": 5, \"name\": \"James\"}]")
+				.toFragment();
+	}
+
+	@Test
+	@PactVerification("provider")
+	public void runTest() {
+		List<Student> students = new Consumer1Application().getStudents();
+
+		assertEquals(1, students.size());
+		assertEquals(5, students.get(0).getId());
+		assertEquals("James", students.get(0).getName());
+	}
+}
