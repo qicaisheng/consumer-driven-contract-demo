ac63e85ce08056704fa2f4f13ec4e8ce52e31d8f
qicaisheng <qicaisheng@qq.com>
2017-05-09 15:39:58 +0800

    Create consumer3 application.

---
+++ consumer3/src/main/java/com/qicaisheng/cdc/consumer3/Consumer3Application.java
@@ -0,0 +1,12 @@
+package com.qicaisheng.cdc.consumer3;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Consumer3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Consumer3Application.class, args);
+	}
+}
