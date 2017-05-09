8354b9ff92bedc4ad867ddadffccb0ee1c576cf9
qicaisheng <qicaisheng@qq.com>
2017-05-09 15:47:57 +0800

    Add contract for consumer3.

---
+++ consumer3/src/main/java/com/qicaisheng/cdc/consumer3/Student.java
@@ -0,0 +1,37 @@
+package com.qicaisheng.cdc.consumer3;
+
+/**
+ * Created by stong on 5/9/17.
+ */
+public class Student {
+    private long id;
+    private String name;
+    private int age;
+
+    public Student() {
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
---
+++ consumer3/src/test/java/com/qicaisheng/cdc/consumer3/Consumer3PactTests.java
@@ -0,0 +1,50 @@
+package com.qicaisheng.cdc.consumer3;
+
+import au.com.dius.pact.consumer.Pact;
+import au.com.dius.pact.consumer.PactProviderRule;
+import au.com.dius.pact.consumer.PactVerification;
+import au.com.dius.pact.consumer.dsl.PactDslWithProvider;
+import au.com.dius.pact.model.PactFragment;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static junit.framework.TestCase.assertEquals;
+
+public class Consumer3PactTests {
+
+	@Rule
+	public PactProviderRule mockProvider = new PactProviderRule("provider", "localhost", 8080, this);
+
+	@Pact(state = "consumer3 get students", provider="provider", consumer="consumer3")
+	public PactFragment createFragment(PactDslWithProvider builder) throws JsonProcessingException {
+		Map<String, String> headers = new HashMap<>();
+		headers.put("Content-Type", "application/json;charset=utf-8");
+
+		return builder
+				.given("contract for consumer3")
+				.uponReceiving("Get students")
+				.path("/students")
+				.method("GET")
+				.willRespondWith()
+				.status(200)
+				.headers(headers)
+				.body("[{\"id\": 5, \"name\": \"James\", \"age\": 18}]")
+				.toFragment();
+	}
+
+	@Test
+	@PactVerification("provider")
+	public void runTest() {
+		List<Student> students = new Consumer3Application().getStudents();
+
+		assertEquals(1, students.size());
+		assertEquals(5, students.get(0).getId());
+		assertEquals("James", students.get(0).getName());
+		assertEquals(18, students.get(0).getAge());
+	}
+}
